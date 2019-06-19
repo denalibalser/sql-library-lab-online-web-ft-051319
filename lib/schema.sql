e7144156feed8554ceacf89d9fbf3cc85a5f7457
Denali Balser <denali.balser167@gmail.com>
2019-06-19 20:49:49 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -15,14 +15,14 @@ series_id  INTEGER
 CREATE TABLE series (id INTEGER PRIMARY KEY,
 title TEXT,
 author_id INTEGER,
-sub_genre_id  INTEGER
+subgenre_id  INTEGER
 );
 
 CREATE TABLE authors (id INTEGER PRIMARY KEY,
 name TEXT
 );
 
-CREATE TABLE sub_genre (id INTEGER PRIMARY KEY,
+CREATE TABLE subgenres (id INTEGER PRIMARY KEY,
 name TEXT
 );
 
